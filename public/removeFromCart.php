02d4fc62f6405878d8fee905a1254af39ad16ceb
CHYGO1985 <jingjiejiang85@gmail.com>
2024-12-07 00:04:57 +0100

    update remove from cart REST endpoint

--- public/removeFromCart.php
+++ public/removeFromCart.php
@@ -1,12 +1,11 @@
 <?php
-include('../includes/functions.php');
+include('includes/functions.php');
 session_start();
 
 // Get book ID from request
 $data = json_decode(file_get_contents('php://input'), true);
 
-if (isset($_SESSION['user_id'], $data['book_id'])) {
-    $user_id = $_SESSION['user_id'];
+if (isset($data['book_id'])) {
     $book_id = $data['book_id'];
 
     // Call the function to remove the book from the cart
